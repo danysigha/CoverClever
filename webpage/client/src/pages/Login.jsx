29b628f727a657ab2cd5e4e097a972137aaee6f4
Calvin Shi <cshi6308@umd.edu>
2025-01-28 17:24:40 -0500

    mock webpage

--- webpage/client/src/pages/Login.jsx
+++ webpage/client/src/pages/Login.jsx
@@ -1,14 +1,32 @@
 import { useState } from "react";
 import axios from "axios";
+import { toast } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+  const navigate = useNavigate();
   const [data, setData] = useState({
     email: "",
     password: "",
   });
 
-  const loginUser = (e) => {
+  const loginUser = async (e) => {
     e.preventDefault();
+    const { email, password } = data;
+    try {
+      const { data } = await axios.post("/login", {
+        email,
+        password,
+      });
+      if (data.error) {
+        toast.error(data.error);
+      } else {
+        setData({});
+        navigate("/dashboard");
+      }
+    } catch (err) {
+      console.log(err);
+    }
     axios.get("/");
   };
 
@@ -24,7 +42,7 @@ export default function Login() {
         />
         <label>Password</label>
         <input
-          type="text"
+          type="password"
           placeholder="Enter password..."
           value={data.password}
           onChange={(e) => setData({ ...data, password: e.target.value })}
--- webpage/client/src/pages/Register.jsx
+++ webpage/client/src/pages/Register.jsx
@@ -1,14 +1,35 @@
 import { useState } from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 export default function Register() {
+  const navigate = useNavigate();
   const [data, setData] = useState({
     name: "",
     email: "",
     password: "",
   });
 
-  const registerUser = (e) => {
+  const registerUser = async (e) => {
     e.preventDefault();
+    const { name, email, password } = data;
+    try {
+      const { data } = await axios.post("/register", {
+        name,
+        email,
+        password,
+      });
+      if (data.error) {
+        toast.error(data.error);
+      } else {
+        setData({});
+        toast.success("Login Successful!");
+        navigate("/login");
+      }
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
@@ -30,7 +51,7 @@ export default function Register() {
         />
         <label>Password</label>
         <input
-          type="text"
+          type="password"
           placeholder="Enter password..."
           value={data.password}
           onChange={(e) => setData({ ...data, password: e.target.value })}
